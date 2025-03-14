8ac4e1f022c089e53a70949a77f76897003fe2bf
PrabhashLiyanage99 <159533393+PrabhashLiyanage99@users.noreply.github.com>
2025-03-14 18:19:50 +0530

    db host

--- server.js
+++ server.js
@@ -17,6 +17,21 @@ app.use(cors({
   credentials: true,
 }));
 
+app.options('*', cors());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://frontend-seven-eta-64.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  next();
+});
+
 app.use(express.json());
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
