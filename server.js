36f47c8b050cd238c8436971651525662d9ec597
PrabhashLiyanage99 <159533393+PrabhashLiyanage99@users.noreply.github.com>
2025-04-27 19:53:57 +0530

    fix

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 // Routes
 app.use(projectsRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 443;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
